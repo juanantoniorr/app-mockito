1c37b9705aea78049ce0f302303a8659aed0888f
juan <juan.antoniorr@hotmail.com>
2021-05-13 11:59:54 -0500

    Using junit with implementation, not ideal

--- src/test/java/org/jrosas/mockito/ejemplos/services/ExamenServiceImplTest.java
+++ src/test/java/org/jrosas/mockito/ejemplos/services/ExamenServiceImplTest.java
@@ -2,6 +2,22 @@ package org.jrosas.mockito.ejemplos.services;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.jrosas.mockito.ejemplos.models.Examen;
+import org.jrosas.mockito.ejemplos.repositories.ExamenRepositoryImpl;
+import org.jrosas.mockito.ejemplos.repositories.IExamenRepository;
+import org.junit.jupiter.api.Test;
+
 class ExamenServiceImplTest {
+	
+	@Test
+	void findExamenByNombre() {
+		
+		IExamenRepository repository = new ExamenRepositoryImpl();
+		IExamenService service = new ExamenServiceImpl(repository);
+		Examen examen = service.findByName("Ciencias");
+		assertNotNull(examen);
+		assertEquals(7, examen.getId());
+		assertEquals("Ciencias", examen.getNombre());
+	}
 
 }
